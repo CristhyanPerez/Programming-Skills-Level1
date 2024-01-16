cb1b9063acb2282fdb72505e171bdf4744e3c0d8
Cristhyan Perez <pevicristhyan@gmail.com>
2024-01-16 11:43:30 -0500

    Update Ex05 - secondary functions - dates, food

--- Turkish_airlines.py
+++ Turkish_airlines.py
@@ -1,5 +1,6 @@
 #Import libraries
 import random
+from datetime import datetime
 
 #List of available options
 available_countries =["Turkey", "Greece", "Lebanon", "Spain", "Portugal"]
@@ -34,77 +35,123 @@ def login_attempts(number_attempts):
                 print("********** Log in **********\n")
     return access_allowed
 
-#Given a menu, a question and a list of answers, the function will evaluate if the
+#Given a question and a list of answers, the function will evaluate if the
 #answer to the question is within the list
-def check_answer(menu, question, list_answers):
-    print(menu)
+def check_answer(question, list_answers):
     start_check = False
     while start_check == False:
         option_user = input(question)
         if option_user in list_answers:
             start_check = True
         else:
-            print("Opcion incorrecta.Let's go again\n")
+            print("Wrong option. Let's go again")
     option_user = int(option_user)
     return option_user
 
-#The countries menu has an exception since the country of departure and return
-#cannot be the same. 1 -> Origin, 2-> Destination
-def country_menu(origin_destination, list_countries):
-    if origin_destination == 1:
-        print("\nAvailable countries of origin:")
+#Define a function to return the country of origin and country of destinatation
+def country_origin_destination(menu):
+    print(menu)
+    origin_country = check_answer("\nChoose the country of origin (1-5): ", available_options)
+    index_in_country = int(origin_country) - 1
+    #Ensure that the country of origin is an option
+    available_options.pop(index_in_country)
+    #With the index we access the country
+    origin_country = available_countries[index_in_country]
+    print("\nNote:")
+    print("The destination country must be different than the origin country")
+    dtn_country = check_answer("\nChoose the country of destination (1-5): ", available_options)
+    index_out_country = int(dtn_country) - 1
+    dtn_country = available_countries[index_out_country]
+    #To return both countries as string
+    return origin_country, dtn_country
+
+#Function to verify the existence of a date of 2024
+def existence_date(date_str):
+    if len(date_str) == 8 and date_str[2] == "-" and date_str[5] == "-":
+        dd = int(date_str[0:2])
+        mm = int(date_str[3:5])
+        aa = int(date_str[6:8])
+        if aa == 24:
+            if mm > 0 and mm <= 12:
+                if mm == 2:
+                    if dd > 0 and dd <= 29:
+                        date_in = True
+                    else:
+                        print("Febrary has a 29 days")
+                        date_in = False
+                elif mm in [1, 3, 5, 7, 8, 10, 12]:
+                    if dd > 0 and dd <= 31:
+                        date_in = True
+                    else:
+                        print("This month has 31 days")
+                        date_in = False
+                elif mm in [4, 6, 9, 11]:
+                    if dd > 0 and dd <= 30:
+                        date_in = True
+                    else:
+                        print("This month has 30 days")
+                        date_in = False
+            else:
+                print("Wrong month")
+                date_in = False
+        else:
+            print("Wrong year")
+            date_in = False
     else:
-        print("\nDestination countries available:")
-    number_countries = len(list_countries)
-    for i in range(number_countries):
-        print(f"{i+1}- {list_countries[i]}")
-    print()
+        print("The date doesn't comply with the format")
+        date_in = False
+    return date_in
 
-#Boarding times will be generated randomly
-def boarding_times_random():
-    print("\nAvailable boarding times:")
-    list_boarding_times = []
-    for i in range(4):
-        minutes = str(random.randint(0,6))
-        if i == 0:
-            hour = str(random.randint(3,6))
-            day_or_night = "a.m"
-        elif i == 1:
-            hour = str(random.randint(9,11))
-            day_or_night = "a.m"
-        elif i == 2:
-            hour = str(random.randint(2,4))
-            day_or_night = "p.m"
-        elif i == 3:
-            hour = str(random.randint(5,8))
-            day_or_night = "p.m"
-        boarding_schedule = hour + ":" + minutes + "0 " + day_or_night
-        list_boarding_times.append(boarding_schedule)
-    return list_boarding_times
+#Function designed to iterate until the user enters a correct date
+def date_loop(sentence):
+    date_in = False
+    while date_in == False:
+        date_format = input(sentence)
+        date_string = date_format.replace(" ", "")
+        existe = existence_date(date_string)
+        if existe == True:
+            dd = int(date_string[0:2])
+            mm = int(date_string[3:5])
+            date_oficial = datetime(2024,mm,dd)
+            return date_oficial
+            break
+        else:
+            print("Retry..!!!\n")
 
-#The schedule menu is generated
-def boarding_times_menu(list_boarding_times):
-    for i in range(4):
-        print(f"{i+1}- {list_boarding_times[i]}")
-    print()
+#Define a function to return the date of departure and the date of return
+def date_dpt_rtn(menu):
+    print(menu)
+    correct_dates = False
+    while correct_dates == False:
+        date_departure = date_loop("\nChoose your departure date: ")
+        date_return = date_loop("\nChoose your return date: ")
+        if date_departure < date_return:
+            correct_dates = True
+            return date_departure, date_return
+        else:
+            correct_dates = False
+            print("Incorrect dates\nRetry..!!!")
 
-#Menu - Economic vs First Class
-menu_class = """
-      Feature      |   Economy class   |   First Class
------------------------------------------------------------
-  Seat space and   |  Narrower seats,  |   wider seats,
-     comfort       |  limited legroom  |   more legroom
- -----------------------------------------------------------
-    Additional     |  Basic services   |   Access to VIP
-    services       |                   | lounges, priority
-                   |                   |     boarding
-"""
+#Function to choose Fist class or economy
+def flight_class(menu):
+    print(menu)
+    question = "Chose the condition, Economic or First class (1-2): "
+    list_answers_class = ["1","2"]
+    option_class = check_answer(question, list_answers_class)
+    if option_class == 1:
+        flight_option = "Economy"
+    else:
+        flight_option = "First Class"
+    return flight_option
 
-#menu to add suitcase
-suitcase_extra ="""
-Notes: 
-* Hand luggage is free of charge.
-* There is a $20.00 charge for additional luggage.
-"""
+#Function to choose favourite food
+def flight_food(menu, options_meal):
+    print(menu)
+    question = "Chose your favourite food (1-3): "
+    list_answers_class = ["1", "2", "3"]
+    option_food = check_answer(question, list_answers_class)
+    index = option_food - 1
+    flight_meal = options_meal[index]
+    return flight_meal
 
 
